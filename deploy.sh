b5b8b9fb2873e1f1e9dd16979a75bb7e07cac7ed
Attila <bitcoinfiles@protonmail.com>
2020-04-23 22:21:01 -0600

    Add cors headers

--- deploy.sh
+++ deploy.sh
@@ -13,5 +13,12 @@ echo "Starting deploy to server... $HOSTNAME1"
 rsync -rav -e "ssh -i $KEYFILEPATH"  --progress --exclude-from='./exclude_files_rsync.txt' ./ ubuntu@$HOSTNAME1:~/merchantapi
 
 
+HOSTNAME1=ec2-18-236-179-129.us-west-2.compute.amazonaws.com
+USERNAME=ubuntu
+KEYFILEPATH="/Users/sauron/git/me/bp_ec2_keypair.pem"
+echo "Starting deploy to server... $HOSTNAME1"
+rsync -rav -e "ssh -i $KEYFILEPATH"  --progress --exclude-from='./exclude_files_rsync.txt' ./ ubuntu@$HOSTNAME1:~/merchantapi
+
+
 echo "\nDone. Bye..."
 exit
--- handler/QueryTransactionStatus.go
+++ handler/QueryTransactionStatus.go
@@ -16,6 +16,8 @@ import (
 
 // QueryTransactionStatus comment
 func QueryTransactionStatus(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With,content-type")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With,Content-Type,Authorization")
 	if r.Method == http.MethodOptions {
